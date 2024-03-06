ecf2a023fbc6e66027280c1f5519d2ea0135ec22
Manatoa <43567527+ManaToa@users.noreply.github.com>
2024-03-07 00:45:20 +0100

    Create button.tsx

---
+++ frontend/button.tsx
@@ -0,0 +1,18 @@
+interface SudokuButtonProps {
+  label: string
+  executeAction: () => void
+}
+
+export default function SudokuButton({
+  label,
+  executeAction,
+}: SudokuButtonProps) {
+  return (
+    <button
+      className='uppercase bg-darkColor text-lightColor p-2 rounded-sm font-bold hover:bg-mainColor'
+      onClick={() => executeAction()}
+    >
+      {label}
+    </button>
+  )
+}
